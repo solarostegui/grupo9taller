2c147b75ad7d3978afb811797a7f4ea85f2a9790
solarostegui <maria.sol.arostegui2000@gmail.com>
2026-06-01 19:19:00 -0300

    Validaciones Estadisticas de grupo

--- src/main/java/org/example/Validador.java
+++ src/main/java/org/example/Validador.java
@@ -9,7 +9,7 @@ public class Validador {
     //necesitamos get y set de partido para entrar a la lista de arbitros de ese partido
     public static boolean validarArbitraje(List <Arbitraje> arbitrajes){
         //La lista tiene que tener algo para que sea válida. Empty es lo mismo que arbitraje.size() == 0 (lista vacia)
-        if (arbitrajes == null || arbitrajes.isEmpty()){
+        if (arbitrajes == null || arbitrajes.isEmpty()){ //Si usamos una excepcion tipo NullPointerException corremos el riesgo de que se craashee el código
             return false;
         }
         boolean tienePrincipal = false;
@@ -53,13 +53,47 @@ public class Validador {
         boolean estaEnLocal = false;
         //Uso el get de mi lista "jugador" en Seleccion
         if(local != null && local.getJugador()!=null){
-            estaEnLocal = local.getJugador().contains(j);
+            estaEnLocal = local.getJugador().contains(j); //Evita anidar bucles o que se repitan datos
         }
         boolean estaEnVisitante = false;
         if(visitante!=null&&visitante.getJugador()!=null){
             estaEnVisitante=visitante.getJugador().contains(j);
         }
         return estaEnLocal || estaEnVisitante;
+    }
+    public static void validarEstadisticasGrupo(Partido p){
+        if (p==null){
+            System.out.println("El partido es nulo.");
+            return;
+        }
+        //Obtener participaciones del partido
+        Participacion local = p.getSeleccion1();
+        Participacion visitante = p.getSeleccion2();
+        //Si local y visitante están vacíos
+        if (local == null || visitante == null){
+            System.out.println("El partido no tiene participaciones cargadas.");
+            return;
+        }
+        //Obtener selecciones
+        Seleccion seleccionLocal = local.getSeleccion();
+        Seleccion seleccionVisitante = visitante.getSeleccion();
+        if (seleccionLocal == null || seleccionVisitante == null){
+            System.out.println("Las participaciones no tienen selecciones asignadas.");
+            return;
+        }
+        //Obtener el grupo al que pertenece la selección local
+        Grupo grupoDelPartido = seleccionLocal.getGrupo();
+        if (grupoDelPartido == null){
+            System.out.println("La selección local no está asignada a ningún grupo.");
+            return;
+        }
+        System.out.println("\n[VALIDADOR] Sincronizando estadísticas del "+ grupoDelPartido.getDescripcion()+".");
+        int ptsLocal = grupoDelPartido.obtenerPuntos(seleccionLocal);
+        int ptsVisitante = grupoDelPartido.obtenerPuntos(seleccionVisitante);
+        System.out.println("[RESULTADOS AUTOMÁTICOS]");
+        System.out.println("- "+seleccionLocal.getNombreFederacion()+": "+ptsLocal+" pts.");
+        System.out.println("- "+seleccionVisitante.getNombreFederacion()+": "+ptsVisitante+" pts.");
+
     }
     
 }
